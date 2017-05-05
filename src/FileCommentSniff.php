7a8aea79ebabd54fbc79fb85032d4bdb54371b0f
narcoticfresh <dn@xbe.ch>
2017-05-05 14:14:50 +0200

    move classes, setup autoloader

--- src/FileCommentSniff.php
+++ src/FileCommentSniff.php
@@ -1,9 +1,10 @@
 <?php
+
+namespace Graviton\Sniff;
+
 /**
  * Ensure that @author annotations contain email or URL
  *
- * PHP version 5
- * 
  * @category  PHP
  * @package   PHP_CodeSniffer
  * @author    Greg Sherwood <gsherwood@squiz.net>
@@ -11,9 +12,9 @@
  * @copyright 2006-2011 Squiz Pty Ltd (ABN 77 084 670 600)
  * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
  * @link      http://pear.php.net/package/PHP_CodeSniffer
- * @class 
+ * @class
  */
-class ENTB_Sniffs_Commenting_FileCommentSniff extends PEAR_Sniffs_Commenting_FileCommentSniff
+class FileCommentSniff extends PEAR_Sniffs_Commenting_FileCommentSniff
 {
     /**
      * Process the author tag(s) that this header comment has.
--- src/ValidInterfaceNameSniff.php
+++ src/ValidInterfaceNameSniff.php
@@ -1,9 +1,10 @@
 <?php
+
+namespace Graviton\Sniff;
+
 /**
  * Ensure that Interfaces are named as they should
  *
- * PHP version 5
- * 
  * @category  PHP
  * @package   PHP_CodeSniffer
  * @author    Greg Sherwood <gsherwood@squiz.net>
@@ -11,9 +12,9 @@
  * @copyright 2006-2011 Squiz Pty Ltd (ABN 77 084 670 600)
  * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
  * @link      http://pear.php.net/package/PHP_CodeSniffer
- * @class 
+ * @class
  */
-class ENTB_Sniffs_NamingConventions_ValidInterfaceNameSniff implements PHP_CodeSniffer_Sniff
+class ValidInterfaceNameSniff implements PHP_CodeSniffer_Sniff
 {
     /**
      * welche tokens wollen wir sniffen
